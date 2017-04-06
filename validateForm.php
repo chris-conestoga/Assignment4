0eb94f0364fe65d2f6bbde1fda16fe316868ac79
chris-conestoga <chris.conetoga@gmail.com>
2017-04-06 17:28:21 -0400

    release candidate

--- validateForm.php
+++ validateForm.php
@@ -42,8 +42,7 @@
 				{
 					$errorMessage=$errorMessage."You must order at least one item.<br/><br/>";
 				}
-				print($first_name);
-				if (!preg_match($nameRegEx,$first_Name))
+				if (!preg_match($nameRegEx,$first_name))
 				{
 					$errorMessage=$errorMessage."Invalid First Name: Must consist of only letters.<br/><br/>";
 				}
@@ -55,10 +54,12 @@
 				{
 					$errorMessage=$errorMessage."Invalid Phone Number: Must consist of only numbers, and no special characters or spaces. Must be between 4 and 16 digits.<br/><br/>";
 				}
+				/*
 				if (!preg_match($emailRegEx,$email))
 				{
 					$errorMessage=$errorMessage."Invalid Email Address: letters, numbers, and special characters such as !#$%&'*+-/=?^_`{|}~ are only allowed in the local part. The domain may only contain letters, numbers, or hyphens [-]. The email address must be formatted as such: ---@----.--- though length may vary.<br/><br/>";
 				}
+				*/
 				if ($street_number=="")
 				{
 					$errorMessage=$errorMessage."You must enter a Street Number / Apartment Number<br/><br/>";
@@ -79,113 +80,117 @@
 				{
 					$errorMessage=$errorMessage."Invalid Postal Code: Must comply with standard Canadian format: e.g. A1A 1A1.<br/><br/>";
 				}
-				if (errorMessage != ""){
+				if ($errorMessage != ""){
 					print('<span id="dialogue"><h1>ERROR:</h1><br/>'.$errorMessage.'</span>');
 				}
 				else
 				{
 					print('<h1>Order recipt:</h1>');
 					print("<br/>Shipping to:");
-			print("<br/>$last_name , $first_name");
-			print("<br/>$street_number $address");
-			print("<br/>$city , $province");
-			print("<br/><br/>Order Information:");
-			$exstand = $standard * 9.95;
-			$explat = $platinum * 29.95;
-			$base_total = $exstand + $explat;
-			if ($base_total > 0 && base_total <= 25)
-			{
-				$delivery_time = 1;
-				$delivery_charge = 3.00;				
-			}
-			elseif ($base_total > 25 && $base_total <= 50)
-			{
-				$delivery_time = 1;
-				$delivery_charge = 4.00;
-			}
-			elseif ($base_total >50 && $base_total <=75)
-			{
-				$delivery_time = 3;
-				$delivery_charge = 5.00;
-			}
-			else
-			{
-				$delivery_time = 4;
-				$delivery_charge = 6.00;
-			}
+					print("<br/>$last_name, $first_name");
+					print("<br/>$street_number $address");
+					print("<br/>$city, $province");
+					print("<br/>$postal_code");
+					print("<br/><br/>Order Information:");
+					$exstand = $standard * 9.95;
+					$explat = $platinum * 29.95;
+					$base_total = $exstand + $explat;
+					if ($base_total > 0 && $base_total <= 25)
+					{
+						$delivery_time = 1;
+						$delivery_charge = 3.00;				
+					}
+					elseif ($base_total > 25 && $base_total <= 50)
+					{
+						$delivery_time = 1;
+						$delivery_charge = 4.00;
+					}
+					elseif ($base_total >50 && $base_total <=75)
+					{
+						$delivery_time = 3;
+						$delivery_charge = 5.00;
+					}
+					else
+					{
+						$delivery_time = 4;
+						$delivery_charge = 6.00;
+					}
 
-			if ($standard > 0)
-			{
-				print("<br/>$standard standard Widgets @ /$9.95 each, extended cost /$$exstand");
-			}
-			if ($platinum > 0)
-			{
-				print("<br/>$platinum platinum Widgets @ /$29.95 each, extended cost /$$explat");
-			}
-			print("<br/>Your base cost is /$$base_total");
-			print("<br/>Your total delivery charge is /$$delivery_charge");
+					if ($standard > 0)
+					{
+						print("<br/>$standard standard Widgets @ $9.95 each, extended cost $$exstand");
+					}
+					if ($platinum > 0)
+					{
+						print("<br/>$platinum platinum Widgets @ $29.95 each, extended cost $$explat");
+					}
+					print("<br/>Your base cost is $$base_total");
+					print("<br/>Your total delivery charge is $$delivery_charge");
 
-			if ($province == "AB")
-			{
-				$prov_tax = $base_total * 0.05;
-			}
-			elseif ($province == "BC")
-			{
-				$prov_tax = $base_total * 0.12;
-			}
-			elseif ($province == "MB")
-			{
-				$prov_tax = $base_total * 0.13;
-			}
-			elseif ($province == "NB")
-			{
-				$prov_tax = $base_total * 0.15;
-			}
-			elseif ($province == "NL")
-			{
-				$prov_tax = $base_total * 0.15;
-			}
-			elseif ($province == "NS")
-			{
-				$prov_tax = $base_total * 0.15;
-			}
-			elseif ($province == "NT")
-			{
-				$prov_tax = $base_total * 0.05;
-			}
-			elseif ($province == "NU")
-			{
-				$prov_tax = $base_total * 0.05;
-			}
-			elseif ($province == "ON")
-			{
-				$prov_tax = $base_total * 0.13;
-			}
-			elseif ($province == "PE")
-			{
-				$prov_tax = $base_total * 0.15;
-			}
-			elseif ($province == "QC")
-			{
-				$prov_tax = $base_total * 0.14975;
-			}
-			elseif ($province == "SK")
-			{
-				$prov_tax = $base_total * 0.11;
-			}
-			elseif ($province == "YT")
-			{
-				$prov_tax = $base_total * 0.05;
-			}
-			else
-			{
-				$prov_tax = $base_total * 1;
-			}
-
-			$total_cost = $base_total + $prov_tax + $delivery_charge;
-			$total_cost = round($total_cost, 2);
-			print("<br/><br/>The total dcost for your order is /$$total_cost");
-			}
+					if ($province == "AB")
+					{
+						$prov_tax = $base_total * 0.05;
+					}
+					elseif ($province == "BC")
+					{
+						$prov_tax = $base_total * 0.12;
+					}
+					elseif ($province == "MB")
+					{
+						$prov_tax = $base_total * 0.13;
+					}
+					elseif ($province == "NB")
+					{
+						$prov_tax = $base_total * 0.15;
+					}
+					elseif ($province == "NL")
+					{
+						$prov_tax = $base_total * 0.15;
+					}
+					elseif ($province == "NS")
+					{
+						$prov_tax = $base_total * 0.15;
+					}
+					elseif ($province == "NT")
+					{
+						$prov_tax = $base_total * 0.05;
+					}
+					elseif ($province == "NU")
+					{
+						$prov_tax = $base_total * 0.05;
+					}
+					elseif ($province == "ON")
+					{
+						$prov_tax = $base_total * 0.13;
+					}
+					elseif ($province == "PE")
+					{
+						$prov_tax = $base_total * 0.15;
+					}
+					elseif ($province == "QC")
+					{
+						$prov_tax = $base_total * 0.14975;
+					}
+					elseif ($province == "SK")
+					{
+						$prov_tax = $base_total * 0.11;
+					}
+					elseif ($province == "YT")
+					{
+						$prov_tax = $base_total * 0.05;
+					}
+					else
+					{
+						$prov_tax = $base_total * 1;
+					}
+					$prov_tax=round($prov_tax,2);
+					print("<br/>Your total taxes: $$prov_tax");
+					$total_cost = $base_total + $prov_tax + $delivery_charge;
+					$total_cost = round($total_cost, 2);
+					print("<br/><br/>The total cost for your order is $$total_cost");
+					$delivery_date = 
+					print("<br/><br/>Your Estimated Delivery Date: ".(date("d")+$delivery_time).date("/m/y"));
+				}
 			}
 			else
 			{
